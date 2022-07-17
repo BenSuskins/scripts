10c15eb468d312f4351b8533de7f5b210594e6f5
Ben Suskins <suskinsdevelopment@gmail.com>
2022-07-17 17:41:35 +0100

    fix order

--- docker/update.sh
+++ docker/update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 for file in *.yml; do
     echo "Updating: $file"
-    docker-compose pull -f $file
-    docker-compose restart -f $file
+    docker-compose -f $file pull
+    docker-compose -f $file restart
 done

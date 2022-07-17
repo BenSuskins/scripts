4e3cb147e4c4c688504ff66e6098d60ce553e477
Ben Suskins <suskinsdevelopment@gmail.com>
2022-07-17 16:52:12 +0100

    Cleanup

--- git/master.sh
+++ git/master.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-repos=("lmd-client-api2" "lmd-schedule-manager" "lmd-tsp2" "lmd-auth2" "lmd-scheduler2" "lmd-sc" "lmd-am" "lmd-media" "lmd-web-app2")
-
-for d in "${repos[@]}"; do
+for d in */ ; do
       echo ""
       echo "-- $d --"
        ( cd "$d" && git co master )
--- git/pull_all.sh
+++ git/pull_all.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-repos=("lmd-client-api2" "lmd-tsp2" "lmd-auth2" "lmd-scheduler2" "lmd-sc" "lmd-am" "lmd-media" "lmd-web-app2" "lmd-ios-app" "lmd-jenkins-files" "lmd-schedule-manager")
-
-for d in "${repos[@]}"; do
+for d in */ ; do
       echo ""
       echo "-- $d --"
       echo -n "Branch: "

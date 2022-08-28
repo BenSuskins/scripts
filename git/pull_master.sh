a0c2b2cb84d2e197e9026e768ae6ae8fc939f343
Ben Suskins <suskinsdevelopment@gmail.com>
2022-08-28 16:07:47 +0100

    master or main

--- git/pull_master.sh
+++ git/pull_master.sh
@@ -2,5 +2,12 @@
 for d in */ ; do
       echo ""
       echo "-- $d --"
-       ( cd "$d" && git co master && git pull)
+      cd "$d"
+      hasMasterBranch=`git show-ref refs/heads/master`
+      if [ -n "$hasMasterBranch" ]; then
+            git co master && git pull
+      else
+            git co main && git pull            
+      fi
+      cd ..
 done

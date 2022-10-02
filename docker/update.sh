5999b5582682ca2a27784620293416e7a6239151
Ben Suskins <suskinsdevelopment@gmail.com>
2022-10-02 11:30:30 +0100

    UPdate

--- docker/update.sh
+++ docker/update.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 for d in */ ; do
+    cd $d
     for file in *.yml; do
         echo ""
         echo "-- $file --"
         docker compose -f $file pull
         docker compose -f $file restart
     done
+    cd ..
 done
